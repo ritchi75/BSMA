662136a8b520b62fd1830b01da32ea21d300ea4f
Yueming Heng <hengy77@students.rowan.edu>
2015-04-05 15:59:16 -0400

    add a location variable in Node

--- BSMA/src/bsma/Node.java
+++ BSMA/src/bsma/Node.java
@@ -17,7 +17,7 @@ public class Node {
     private Node rightChild; // the left and right child of any node are buddies
     private int size; // size of this particular chunk of memory(power of 2)
     private Data data; // represents the data that is stored in this chunk
-
+    private int location;
     /**
      * Constructor for root of new tree.
      *
@@ -55,7 +55,9 @@ public class Node {
         leftChild = left;
         rightChild = right;
     }
-
+    public void setLocation(int loca){
+        this.location = loca;
+    }
     /**
      * Deletes both of the Nodes children
      */
